f2c75b34c6405bb697271ea46f64179b34dd89db
barone04 <tbao041024@gmail.com>
2026-06-25 14:26:27 +0700

    smoke test

--- scripts/run_e2e.sh
+++ scripts/run_e2e.sh
@@ -24,12 +24,12 @@ PY="${PY:-python}"
 
 # Pruning hyper-params
 TARGET_SPARSITY="${TARGET_SPARSITY:-0.5}"
-PRUNE_ITERS="${PRUNE_ITERS:-7}"
-FINETUNE_EPOCHS="${FINETUNE_EPOCHS:-6}"
+PRUNE_ITERS="${PRUNE_ITERS:-2}"
+FINETUNE_EPOCHS="${FINETUNE_EPOCHS:-3}"
 SCOPE="${SCOPE:-all}"                   # all | backbone | neck (component-aware)
 
-DENSE_EPOCHS="${DENSE_EPOCHS:-60}"
-FINAL_EPOCHS="${FINAL_EPOCHS:-60}"
+DENSE_EPOCHS="${DENSE_EPOCHS:-5}"
+FINAL_EPOCHS="${FINAL_EPOCHS:-5}"
 
 WANDB="${WANDB:-0}"                     # WANDB=1 de bat log wandb (env key da set san tren FPT)
 WANDB_ARG=""; [ "$WANDB" = "1" ] && WANDB_ARG="--wandb"
